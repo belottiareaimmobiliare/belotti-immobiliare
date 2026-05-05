c262f0a6d58f7a69c12e79fd2422f953c65a595b
Omar Martalo <omar@MacBookPro.lan>
2026-05-05 20:53:37 +0200

    hide WhatsApp widget on expanded map page

--- src/components/RouteBodyClass.tsx
+++ src/components/RouteBodyClass.tsx
@@ -8,13 +8,16 @@ export default function RouteBodyClass() {
 
   useEffect(() => {
     const isHome = pathname === '/'
+    const isMapArea = pathname?.startsWith('/immobili/mappa-area') || false
 
     document.body.classList.toggle('is-home-route', isHome)
     document.body.classList.toggle('is-non-home-route', !isHome)
+    document.body.classList.toggle('is-map-area-route', isMapArea)
 
     return () => {
       document.body.classList.remove('is-home-route')
       document.body.classList.remove('is-non-home-route')
+      document.body.classList.remove('is-map-area-route')
     }
   }, [pathname])
 
